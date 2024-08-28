5e50cbebc8c049eb7da2f5f051fb40edb83ca262
Ahd Ghazal <ahdgh02@gmail.com>
2024-08-28 19:39:36 +0300

    admin functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom'; // Only import Route and Routes
 import Header from './Header';
 import Home from './Home';
 import TryNow from './TryNow';
@@ -9,22 +9,20 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <video className="background-video" autoPlay loop muted>
-          Your browser does not support the video tag.
-        </video>
-        <div className="content">
-          <Header />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/try-now" element={<TryNow />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-            <Route path="/admin-dashboard" element={<AdminDashboard />} />
-          </Routes>
-        </div>
+    <div className="App">
+      <video className="background-video" autoPlay loop muted>
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/try-now" element={<TryNow />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        </Routes>
       </div>
-    </Router>
+    </div>
   );
 }
 
